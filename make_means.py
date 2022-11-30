54bad142fcb38f78c3ce90bb5463c2d660eb48d0
grleung <gabrielle.leung@colostate.edu>
2022-11-30 08:54:24 -0700

    post-processing for plotting scripts

--- make_means.py
+++ make_means.py
@@ -6,16 +6,17 @@ import time
 from shared_functions import get_var
 from run_params import *
 
-h = 12
+h0 = 4
+h =12
 
-run = 'nograd.1000.absc'
+run = 'grad.absc.mid'
+ccn_name = 'BRC1MP'
+
 anaPath = f"/camp2e/gleung/aerograd-analysis/"
-
-dx  = 100
 
 wc = pd.read_pickle(f"{anaPath}{run}/mean_cross_section/WC.pkl").reset_index()
 uc = pd.read_pickle(f"{anaPath}{run}/mean_cross_section/VC.pkl").reset_index()
-ccn = pd.read_pickle(f"{anaPath}{run}/mean_cross_section/BRC1MP.pkl").reset_index()
+ccn = pd.read_pickle(f"{anaPath}{run}/mean_cross_section/{ccn_name}.pkl").reset_index()
 ccn = ccn[ccn.time<=h*12+1]
 wc = wc[wc.time<=h*12 + 1]
 uc = uc[uc.time<=h*12 + 1]
@@ -44,16 +45,14 @@ fthrd = fthrd[fthrd.time<=h*12 + 1]
 
 data = tcon.copy()
 data['WC'] = wc.WC
-#data = wc.copy()
-data['BRC1MP'] = ccn.BRC1MP
+data[ccn_name] = ccn[ccn_name]
 data['VC'] = uc.VC
 data['y'] = ((data.y+1)*dx)/1000
 data['y'] = data.y+0.05
-#data = data[(data.y>=50) & (data.y<=9950)]
 
 data.columns = ['time','z','y',
                 'TCON',
-                'WC','VC']
+                'WC',ccn_name,'VC']
 data['absx'] = abs(data.y-50)
 data.loc[data.y-50<0,'VC'] = -data[data.y-50<0].VC 
 data['SWDN'] = swdn.SWDN
@@ -64,7 +63,7 @@ data['FTHRD'] = fthrd.FTHRD
 data['absx'] = 0.05 * (data.absx//0.05)
 
 
-data = data[(data.time <= 12*h + 1) & (data.time>12*h1)]
+data = data[(data.time>h0*12) & (data.time <= 12*h + 1)]
 mean = data.groupby(['time','z','absx']).mean()
 mean = mean.reset_index()
 plot = mean.groupby(['z','absx']).mean().reset_index()
@@ -79,16 +78,35 @@ winds = winds.reset_index()
 
 winds = winds[(winds.alt>0) & (winds.alt<=14)]
 
-winds.to_pickle(f"{anaPath}{run}/mean_wind-{h}.pkl")
+winds.to_pickle(f"{anaPath}{run}/mean_wind-{h0}-{h}.pkl")
 resz = 1
 resx = 0.25
 
 plot = plot.reset_index()
 plot = plot.groupby([resx*round(plot.absx//resx), 'z']).mean()[['TCON',
-                                                                'WC','BRC1MP',
+                                                                'WC',ccn_name,
                                                                 'SWDN','SWUP',
                                                                 'LWDN','LWUP',
                                                                 'FTHRD']]
 plot = plot.reset_index()
 
-plot.to_pickle(f"{anaPath}{run}/mean_cross_section-{h}.pkl")
+plot.to_pickle(f"{anaPath}{run}/mean_cross_section-{h0}-{h}.pkl")
+
+aod = pd.read_pickle(f"{anaPath}{run}/mean_surf_flux/AODT.pkl").reset_index()
+shf = pd.read_pickle(f"{anaPath}{run}/mean_surf_flux/SFLUX_T.pkl").reset_index() 
+lhf = pd.read_pickle(f"{anaPath}{run}/mean_surf_flux/SFLUX_R.pkl").reset_index()
+
+surf = aod.copy()
+
+surf['SHF'] = shf.SFLUX_T * cp
+surf['LHF'] = lhf.SFLUX_R * lv
+resx = 0.25
+
+suba = surf[(surf.time<=12*h+1) & (surf.time>12*h0)]
+suba['y'] = (((suba.y+1)*dx)/1000) + 0.05
+suba['absx'] = resx * (abs(suba.y - 50) // resx)
+
+suba = suba.groupby('absx').mean()
+
+suba.to_pickle(f"{anaPath}{run}/mean_surf_flux-{h0}-{h}.pkl")
+
---
+++ make_timeseries.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import numpy as np
+import datetime as dt
+from run_params import dx,dy,nx,ny
+
+runs = ['grad.1000','nograd.1000','grad.500','nograd.500']
+
+anaPath = f"/camp2e/gleung/aerograd-analysis/"
+
+
+def timeseries_features(run, pcp_thresh=0.1, hours=12):
+    df = pd.read_hdf(f"{anaPath}{run}/tobac-out/w_features_track.h5",'table')
+    df['mean_pcprr'] = df.feature.map(pd.read_pickle(f"{anaPath}{run}/tobac-out/pcprr_features.pkl").set_index('feature').mean_pcprr)
+    df['maxcell_pcprr'] = df.cell.map(df.groupby('cell').mean_pcprr.mean())
+    
+    #minimum rainthreshold
+    df = df[df.maxcell_pcprr*3600>pcp_thresh]
+    
+    #lifetime threshold
+    df = df[df.lifetime>dt.timedelta(minutes=5)]
+    
+    df = df.groupby('frame').feature.count()
+    df = df[df.index<=hours*12 + 1]
+    
+    df = df.append(pd.Series(np.zeros(df.index[0]))).sort_index()
+    
+    return(df)
+
+def timeseries_totalrain(run, hours=12):
+    df = pd.read_pickle(f"{anaPath}{run}/mean_surf_flux/ACCPR.pkl").reset_index()
+    df = df.groupby('time').ACCPR.mean()
+
+    df = df[df.index<12*hours +1] * (dx * dy *nx * ny)/1E9
+    
+    return(df)
+
+for run in runs:
+    df = pd.DataFrame()
+    df['RainingUpdrafts'] = timeseries_features(run)
+    df['AccRain'] = timeseries_totalrain(run)
+    
+    df.to_pickle(f"{anaPath}{run}/timeseries.pkl")
